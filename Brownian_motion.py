82b9acb5d6893f2fa04d24eaed6691f0e165b0c7
tynski <b.tynski@gmail.com>
2019-03-05 20:26:28 +0100

    Brownian final

--- Brownian_motion.py
+++ Brownian_motion.py
@@ -1,51 +1,42 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[69]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 
-get_ipython().run_line_magic('matplotlib', 'inline')
+# set size for plots
 plt.rcParams['figure.figsize'] = (14.0, 8.0)
 
-
-# In[75]:
-
-
-N = 1000
+# initial values
+n_steps = 1000
 n_parts = 100
-
-x = np.zeros((N, n_parts))
-y = np.zeros((N, n_parts))
-
 mu, sigma = 0, 1
 
-for i in range(N - 1):
+# booleans for drawing plots
+plot_motion = False
+plot_disp = True
+
+# initialize x and y
+x = np.zeros((n_steps, n_parts))
+y = np.zeros((n_steps, n_parts))
+
+
+for i in range(n_steps - 1):
     dx = np.random.normal(mu, sigma, (1, n_parts))
     dy = np.random.normal(mu, sigma, (1, n_parts))
     x[i + 1, :] = x[i, :] + dx
     y[i + 1, :] = y[i, :] + dy
 
+if plot_motion:
+    plt.plot(x, y)
+    plt.xlabel('time step', fontsize=14)
+    plt.ylabel('particle motion', fontsize=14)
+    plt.title('Brownian motion for particles', fontsize=16)
+    plt.show()
 
-# In[73]:
+# mean square of displacement
+disp = x**2 + y**2
+disp_ms = np.mean(disp, axis=1)
 
-
-plt.plot(x, y)
-plt.xlabel('time step', fontsize=14)
-plt.ylabel('particle motion', fontsize=14)
-plt.title('Brownian motion for particles', fontsize=16)
-
-
-# In[76]:
-
-
-d_square = x**2 + y**2 
-d_square = np.mean(d_square, axis=1)
-plt.plot(range(N), d_square)
-plt.ylabel('mean square of displacement', fontsize=14)
-plt.xlabel('number of steps', fontsize=14)
-
-
+if plot_disp:
+    plt.plot(range(n_steps), disp_ms)
+    plt.ylabel('mean square of displacement', fontsize=14)
+    plt.xlabel('number of steps', fontsize=14)
+    plt.show()
